6cf611165061779d03e8c9430d45a8cd29a6cf28
wsls199033 <493021638@qq.com>
2018-12-26 15:58:18 +0800

    增加了些注释

--- day64_01/src/main/java/com/tarena/groupon/ui/MainActivity.java
+++ day64_01/src/main/java/com/tarena/groupon/ui/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(
-                R.layout.activity_main);
+        //多点注释
+        setContentView(R.layout.activity_main);
     }
 }
